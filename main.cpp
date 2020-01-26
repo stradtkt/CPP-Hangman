ec913610d3d194360db975e8f2a7aa1ce6578a5a
Kevin Stradtman <stradtkt22@gmail.com>
2020-01-26 16:36:17 -0500

    Added some checking to see what the guess count is

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ void printMessage(string message, bool printTop = true, bool printBottom = true)
     if(printTop) {
         cout << "+-------------------------------------+" << endl;
         cout << "|";
+    } else {
+        cout << "|";
     }
     bool front = true;
     for(int i = message.length(); i < 39; i++) {
@@ -19,12 +21,55 @@ void printMessage(string message, bool printTop = true, bool printBottom = true)
     if(printBottom) {
         cout << "|" << endl;
         cout << "+-------------------------------------+" << endl;
+    } else {
+        cout << "|" << endl;
     }
 }
 
-void drawHangman() {}
+void drawHangman(int guessCount = 0) {
+    if(guessCount >= 1) {
+        printMessage("|", false, false);
+    } else {
+        printMessage("", false, false);
+    }
+    if(guessCount >= 2) {
+        printMessage("|", false, false);
+    } else {
+        printMessage("", false, false);
+    }
+    if(guessCount >= 3) {
+        printMessage("0", false, false);
+    } else {
+        printMessage("", false, false);
+    }
+    if(guessCount == 4) {
+        printMessage("/", false, false);
+    }
+    if(guessCount == 5) {
+        printMessage("/|", false, false);
+    }
+    if(guessCount >= 6) {
+        printMessage("/|\\", false, false);
+    } else {
+        printMessage("", false, false);
+    }
+    if(guessCount >= 7) {
+        printMessage("|", false, false);
+    } else {
+        printMessage("", false, false);
+    }
+    if(guessCount == 8) {
+        printMessage("/", false, false);
+    }
+    if(guessCount >= 9) {
+        printMessage("/\\", false, false);
+    } else {
+        printMessage("", false, false);
+    }
+}
 
 int main() {
     printMessage("HANG MAN");
+    drawHangman(9);
     return 0;
 }
